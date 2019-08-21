2fa74fcd2c707d7fa46c937ac26d5e5226af73d3
Kevin Colten <kevincolten@gmail.com>
2019-08-21 15:43:21 -0500

    other format

--- _plugins/custom_filters.rb
+++ _plugins/custom_filters.rb
@@ -2,7 +2,7 @@ module Jekyll
   module CustomTagFilter
     def phone_number(phone)
       phone = phone.to_s
-      "(#{phone[1, 3]}) #{phone[4, 3]}-#{phone[7, 4]}"
+      "#{phone[1, 3]}-#{phone[4, 3]}-#{phone[7, 4]}"
     end
   end
 end
