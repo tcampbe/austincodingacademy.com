cdf0681b5fa5059ff17bf51214c6913d184003cc
Kevin Colten <kevincolten@gmail.com>
2020-01-23 16:33:05 -0600

    deployments

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+git config --global user.email "kevin@austincodingacademy.com"
+git config --global user.name "Kevin Colten"
+if [ "${CIRCLE_BRANCH}" == "preview" ]; then export JEKYLL_ENV='preview.'; fi
+if [ "${CIRCLE_BRANCH}" == "preview" ] || [ "${CIRCLE_BRANCH}" == "master" ]; then
+  yarn images
+  yarn configs
+  node _javascripts/github.js
+  KEY=austincodingacademy.com yarn jekyll-build-amp
+  yarn optimize
+  yarn css
+  ITER=0
+  for file in ./_configs/*; do
+    if [[ -f $file ]] && [[ $(($ITER % $CIRCLE_NODE_TOTAL)) == $CIRCLE_NODE_INDEX ]]; then
+      export KEY=$(echo $file | sed "s/^.\/_configs\/\(.*\).yml$/\1/")
+      yarn jekyll-build
+      yarn favicon
+      if [ "${CIRCLE_BRANCH}" == "master" ]; then yarn sitemap; fi
+      yarn jekyll-build
+      yarn jekyll-build-amp
+      yarn optimize
+      if [ "${CIRCLE_BRANCH}" == "preview" ]; then yarn encrypt; fi
+      if [ "${KEY}" == "austincodingacademy.com" ]
+      then
+        sudo apt install rsync
+        rsync -a --delete -e "ssh -o StrictHostKeyChecking=no" _site/ root@134.209.57.190:/usr/share/nginx/html
+      else
+        yarn deploy
+      fi
+    fi
+    ((ITER++))
+  done
+fi
